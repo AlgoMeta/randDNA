27028f0688a0176d9fc2ef0a94810f83c215875e
AlgoMeta <AlgoMeta@users.noreply.github.com>
2019-02-13 23:10:50 -0400

    hpp file errors fixed

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,16 +1,17 @@
+#include <string>
 #include <random>
 
 
-string randDNA(int seed, string bases, int n)
+std::string randDNA(int seed, std::string bases, int n)
 {
-	string DNA = "";
+	std::string DNA = "";
 	int basesLength = bases.length();
 	std::mt19937 engine(seed);
 	std::uniform_int_distribution<> uniDis(0, (basesLength - 1));
 
 	for (int x = 0; x < n; x++)
 	{
-		DNA = DNA + bases.at(uniDis(eng2));
+		DNA = DNA + bases.at(uniDis(engine));
 	}
 	
 	return DNA;
