0fe5e408daa406ee9f5e01f9ac2208b851a4a218
AlgoMeta <AlgoMeta@users.noreply.github.com>
2019-02-14 17:39:13 -0400

    header passes both tests

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -9,9 +9,12 @@ std::string randDNA(int seed, std::string bases, int n)
 	std::mt19937 engine(seed);
 	std::uniform_int_distribution<> uniDis(0, (basesLength - 1));
 
-	for (int x = 0; x < n; x++)
+	if(bases != "")
 	{
-		DNA = DNA + bases.at(uniDis(engine));
+		for (int x = 0; x < n; x++)
+		{
+			DNA = DNA + bases.at(uniDis(engine));
+		}
 	}
 	
 	return DNA;
